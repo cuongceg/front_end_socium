9d1e5d6b646cca0595d06039cc89e06142e35f0c
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2023-08-25 10:39:08 +0700

    da cap nhat lai mau appbar

--- lib/Widgets/HomePage.dart
+++ lib/Widgets/HomePage.dart
@@ -1,3 +1,5 @@
+import 'package:app/SideBar.dart';
+import 'package:app/Widgets/Proflie.dart';
 import 'package:get/get.dart';
 import 'package:app/Login/SignIn.dart';
 import 'package:flutter/material.dart';
@@ -7,13 +9,12 @@ class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
-
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
   static List<Widget> _widgetOptions = <Widget>[
     const Text("trang 1"),
     const Text('trang 2'),
-    const Text('trang 3')
+    const MyProfileWidget()
   ];
   void _onItemTapped(int index) {
     setState(() {
@@ -23,34 +24,25 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.purple[200],
-          leading: Builder(
-              builder: (BuildContext context){
-                return IconButton(
-                    icon:const Icon(Icons.notifications_active),
-                    onPressed: (){
-                      Navigator.of(context).restorablePush(showAlert);
-                    }
-                );
-              }
-          ),
+      drawer: SlideBar(),
+      appBar: AppBar(
+          backgroundColor: Colors.purple[50],
           title: const Text('Hello'),
           actions: [
             IconButton(
               icon:const Icon(Icons.logout),
               onPressed: () async{
-                Get.to(Login());
+                Get.to(() => const Login());
               },
               tooltip: 'log out',
             )
           ],
         ),
-        backgroundColor: Colors.purple[50],
-        body: Center(
+      backgroundColor: Colors.purple[50],
+      body: Center(
           child: _widgetOptions.elementAt(_selectedIndex),
         ),
-        bottomNavigationBar: BottomNavigationBar(
+      bottomNavigationBar: BottomNavigationBar(
           items: <BottomNavigationBarItem>[
             BottomNavigationBarItem(
                 icon: Icon(Icons.search_sharp,color:Colors.purple[100],),
