e717e8e78c6ba1227e141b8b68ecdd42b8c4f5de
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2023-08-28 23:07:36 +0700

    cap nhat HomePage

--- lib/Widgets/HomePage.dart
+++ lib/Widgets/HomePage.dart
@@ -1,8 +1,8 @@
-import 'package:app/SideBar.dart';
-import 'package:app/Widgets/Proflie.dart';
-import 'package:get/get.dart';
-import 'package:app/Login/SignIn.dart';
+import 'package:app/Widgets/SearchPage.dart';
+import 'package:app/Widgets/SideBar.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -10,88 +10,37 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 class _HomePageState extends State<HomePage> {
-  int _selectedIndex = 0;
-  static List<Widget> _widgetOptions = <Widget>[
-    const Text("trang 1"),
-    const Text('trang 2'),
-    const MyProfileWidget()
-  ];
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: SlideBar(),
+      drawer: const SlideBar(),
       appBar: AppBar(
-          backgroundColor: Colors.purple[50],
-          title: const Text('Hello'),
-          actions: [
-            IconButton(
-              icon:const Icon(Icons.logout),
-              onPressed: () async{
-                Get.to(() => const Login());
+        backgroundColor: Colors.purple[50],
+        title:Text('Hello cuongceg',style: GoogleFonts.roboto(fontSize: 20,color: Colors.black54)),
+        centerTitle: true,
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(
+                Icons.menu_open,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
               },
-              tooltip: 'log out',
-            )
-          ],
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+            );
+          },
         ),
-      backgroundColor: Colors.purple[50],
-      body: Center(
-          child: _widgetOptions.elementAt(_selectedIndex),
-        ),
-      bottomNavigationBar: BottomNavigationBar(
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-                icon: Icon(Icons.search_sharp,color:Colors.purple[100],),
-                label: 'Search',
-                tooltip:'Search people.'
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.messenger,color:Colors.purple[100],),
-                label: 'Chat',
-                tooltip:'Chat',
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.person,color:Colors.purple[100],),
-                tooltip: 'Profile',
-                label: 'Profile'
-            )
-          ],
-          currentIndex: _selectedIndex,
-          selectedItemColor: Colors.blueAccent,
-          onTap: _onItemTapped,
-        ),
-      );
-  }
-  @pragma('vm:entry-point')
-  static Route<Object?> showAlert(BuildContext context, Object? arguments) {
-    return DialogRoute<void>(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Notifications'),
-          actions: <Widget>[
-            TextButton(
-                style: TextButton.styleFrom(
-                  textStyle: Theme.of(context).textTheme.labelLarge,
-                ),
-                onPressed:(){
-                  Navigator.pop(context);
-                } ,
-                child: const Text(
-                  'Back',
-                  style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.black
-                  ),
-                )
-            )
-          ],
-        );
-      },
+        actions: [
+          IconButton(
+              onPressed:(){
+              },
+              icon: const Icon(Icons.message_outlined,color: Colors.black,))
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: SearchPage()
     );
   }
 }
