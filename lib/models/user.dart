dbe31b75c161b0f4c480455c4d35ff15215cb08b
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2023-08-29 17:07:49 +0700

    them class Auth

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,6 +1,10 @@
 import 'dart:io';
 class User{
-  final File? photochoose;
   final String? email,adress,school,age,cpa,gender;
-  User(this.email, this.adress, this.school, this.age, this.cpa, this.gender, this.photochoose);
+  User(this.email, this.adress, this.school, this.age, this.cpa, this.gender);
+}
+class Auth{
+  final String name,username,image;
+  bool isFollowedByMe;
+  Auth(this.name, this.username, this.image, this.isFollowedByMe);
 }
