e0b6d0f320b516047fc748eb66be3f0699a28451
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2023-08-29 17:47:31 +0700

    them giao dien dang nhap bang google

--- lib/Login/SignIn.dart
+++ lib/Login/SignIn.dart
@@ -1,9 +1,11 @@
+import 'package:app/Login/SignInWithUsername.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'SignUp.dart';
 import 'package:app/Widgets/Loading.dart';
 import 'package:app/Widgets/HomePage.dart';
 import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 class Login extends StatefulWidget{
   const Login({Key? key}) : super(key: key);
 
@@ -35,91 +37,25 @@ class _LoginState extends State<Login>{
                       key: _formkey,
                       child:ListView(
                         children:<Widget>[
+                          Container(
+                            color: Colors.deepPurple[200],
+                            width: widthR,
+                            height: heightR/20,
+                          ),
                           SizedBox(
                             height:heightR/4,
                             width: widthR/4,
-                            child: Center(child: Image.asset('assets/images/2.png',width:widthR/3.5,height: heightR/3.5,)),
+                            child: Center(child: Image.asset('assets/images/2.png',width:widthR/3,height: heightR/3,)),
                           ),
-                          Padding(
-                            padding: const EdgeInsets.all(20.0),
-                              child: TextFormField(
-                                controller: UserNameEditingController,
-                                onChanged: (text){
-                                  setState(() {
-                                    _username=text;
-                                  });
-                                },
-                                decoration: const InputDecoration(
-                                    enabledBorder: OutlineInputBorder(
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(20)),
-                                        borderSide: BorderSide(color: Colors.deepPurple,width:2.0)
-                                    ),
-                                    focusedBorder: OutlineInputBorder(
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(20)),
-                                        borderSide: BorderSide(color: Colors.deepPurple,width:2.0)
-                                    ),
-                                    fillColor: Colors.white,
-                                    filled: true,
-                                    labelText:'User name'
-                                ),
-                              ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(20.0),
-                            child: SizedBox(
-                              height: heightR/13,
-                              width: widthR,
-                              child: TextFormField(
-                                controller:passwordEditingController ,
-                                obscureText: hint,
-                                onChanged: (text){
-                                  setState(() {
-                                    _password=text;
-                                  });
-                                },
-                                decoration: InputDecoration(
-                                  enabledBorder: const OutlineInputBorder(
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(20)),
-                                      borderSide: BorderSide(color: Colors.deepPurple,width:2.0)
-                                  ),
-                                  focusedBorder: const OutlineInputBorder(
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(20)),
-                                      borderSide: BorderSide(color: Colors.deepPurple,width:2.0)
-                                  ),
-                                  fillColor: Colors.white,
-                                  filled: true,
-                                  labelText:'Password',
-                                  suffix: IconButton(
-                                      icon:hint?const Icon(Icons.remove_red_eye_outlined):const Icon(Icons.visibility_off),
-                                      onPressed:()async{
-                                        setState(() {
-                                          toggleView();
-                                        });
-                                      }
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          const Padding(
-                            padding: EdgeInsets.only(bottom: 10,top: 5),
-                            child: SizedBox(height: 12.0,width: 30.0),
-                          ),
-                          Text(
-                            error,
-                            style: TextStyle(
-                                color: Colors.red[500],
-                                fontSize: 14
-                            ),
+                          Container(
+                            color: Colors.deepPurple[200],
+                            width: widthR,
+                            height: heightR/6,
                           ),
                           Padding(
                             padding: const EdgeInsets.symmetric(vertical: 5,horizontal: 40),
                             child: SizedBox(
-                              height: heightR/15,
+                              height: heightR/13,
                               width: widthR/2,
                               child: Container(
                                 decoration: const BoxDecoration(
@@ -131,18 +67,89 @@ class _LoginState extends State<Login>{
                                     setState(() {
                                       loading=true;
                                     });
-                                    await Navigator.push(context, MaterialPageRoute(builder: (context) => const HomePage()));
+                                    Get.to(() => const LoginWithUsername());
                                     // dynamic result= await _auth.signInemailandpassword(_email, _password);
                                     // if(result == null){
                                     //   setState(() => error ='Invalid email or wrong password,please try again!' );
                                     //   loading=false;
                                     //}
                                   },
-                                  child:Text(
-                                    "Login",
-                                    style: GoogleFonts.roboto(color: Colors.black,
-                                      fontSize: 25,)
-                                  ),
+                                  child:ListTile(
+                                    title: Text(
+                                        "LOGIN WITH USERNAME",
+                                        style: GoogleFonts.roboto(color: Colors.black,
+                                          fontSize: 18,)
+                                    ),
+                                    leading: const Icon(Icons.person,color: Colors.blue,),
+                                  )
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 15,horizontal: 40),
+                            child: SizedBox(
+                              height: heightR/13,
+                              width: widthR/2,
+                              child: Container(
+                                decoration: const BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.all(Radius.circular(20))
+                                ),
+                                child: TextButton(
+                                    onPressed:() async{
+                                      setState(() {
+                                        loading=true;
+                                      });
+                                      await Get.to(() => const HomePage());
+                                      // dynamic result= await _auth.signInemailandpassword(_email, _password);
+                                      // if(result == null){
+                                      //   setState(() => error ='Invalid email or wrong password,please try again!' );
+                                      //   loading=false;
+                                      //}
+                                    },
+                                    child:ListTile(
+                                      title: Text(
+                                          "LOGIN WITH FACEBOOK",
+                                          style: GoogleFonts.roboto(color: Colors.black,
+                                            fontSize: 18,)
+                                      ),
+                                      leading: const Icon(Icons.facebook,color: Colors.blue,),
+                                    )
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 5,horizontal: 40),
+                            child: SizedBox(
+                              height: heightR/13,
+                              width: widthR/2,
+                              child: Container(
+                                decoration: const BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.all(Radius.circular(20))
+                                ),
+                                child: TextButton(
+                                    onPressed:() async{
+                                      setState(() {
+                                        loading=true;
+                                      });
+                                      await Get.to(() => const HomePage());
+                                      // dynamic result= await _auth.signInemailandpassword(_email, _password);
+                                      // if(result == null){
+                                      //   setState(() => error ='Invalid email or wrong password,please try again!' );
+                                      //   loading=false;
+                                      //}
+                                    },
+                                    child:ListTile(
+                                      title: Text(
+                                          "LOGIN WITH GOOGLE",
+                                          style: GoogleFonts.roboto(color: Colors.black,
+                                            fontSize: 18,)
+                                      ),
+                                      leading: Image.asset('assets/images/google.png',width: 20,height: 20,),
+                                    )
                                 ),
                               ),
                             ),
@@ -151,15 +158,14 @@ class _LoginState extends State<Login>{
                               height: heightR/5,
                               width: widthR,
                               child:Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
+                                mainAxisAlignment: MainAxisAlignment.center,
                                 children: <Widget>[
                                   Text(
                                     'Don\'t have account ?',
                                     style: GoogleFonts.roboto(color: Colors.white,fontSize: 22)
                                   ),
                                   TextButton(
-                                      onPressed:() =>
-                                          Navigator.push(context, MaterialPageRoute(builder: (context) => SignUp())),
+                                      onPressed:() =>Get.to(()=>SignUp()),
                                       child: Text(
                                         "Sign up",
                                         style: GoogleFonts.roboto(color: Colors.white,fontSize: 25,fontWeight: FontWeight.bold)
