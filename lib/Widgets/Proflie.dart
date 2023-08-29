102c67dd965a1412dc76d6223d47eb9340a44f76
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2023-08-29 11:10:31 +0700

    cap nhat trang edit account settings

--- lib/Widgets/Proflie.dart
+++ lib/Widgets/Proflie.dart
@@ -1,15 +1,18 @@
 import 'dart:io';
+import 'package:app/Widgets/HomePage.dart';
+import 'package:app/models/user.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
 class MyProfileWidget extends StatefulWidget {
   const MyProfileWidget({super.key});
-
   @override
   State<MyProfileWidget> createState() => _MyProfileWidgetState();
 }
-
-class _MyProfileWidgetState extends State<MyProfileWidget> {
+class _MyProfileWidgetState extends State<MyProfileWidget>with TickerProviderStateMixin{
+  late AnimationController controller;
+  bool load=false;
   String? address,_email,_password,_confirmpassword,school,age,cpa;
   String? gender_choose;
   bool hint =true;
@@ -31,7 +34,16 @@ class _MyProfileWidgetState extends State<MyProfileWidget> {
     double heightR=MediaQuery.of(context).size.height;
     double widthR=MediaQuery.of(context).size.width;
     return Scaffold(
-      backgroundColor: Colors.purple[50],
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: IconButton(
+            icon:const Icon(Icons.arrow_back,size: 30,color: Colors.black,),
+            onPressed: (){
+              Get.to(()=>const HomePage());
+            },
+        ),
+      ),
+      backgroundColor: Colors.white,
       body: Form(
         key: _formkey,
           child: ListView(
@@ -40,43 +52,17 @@ class _MyProfileWidgetState extends State<MyProfileWidget> {
               Padding(
                 padding: const EdgeInsets.all(10.0),
                 child: TextFormField(
-                  controller: emailEditingController,
-                  onChanged: (text){
-                    setState(() {
-                      _email=text;
-                    });
-                    },
-                  decoration: const InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(
-                              Radius.circular(20)),
-                          borderSide: BorderSide(color: Colors.deepPurple,width:2.0)
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(
-                              Radius.circular(20)),
-                          borderSide: BorderSide(color: Colors.deepPurple,width:2.0)
-                                    ),
-                                    fillColor: Colors.white,
-                                    filled: true,
-                                    labelText:'Email Address'
-                                ),
-                              ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextFormField(
                   controller: adressEditingController,
                   onChanged: (text){
                     setState(() {
                       address=text;
                     });
-                    },
+                  },
                   decoration: const InputDecoration(
                       enabledBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.all(
                               Radius.circular(20)),
-                          borderSide: BorderSide(color: Colors.deepPurple,width:2.0)
+                          borderSide: BorderSide(color: Colors.black,width:2.0)
                       ),
                       focusedBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.all(
@@ -91,89 +77,11 @@ class _MyProfileWidgetState extends State<MyProfileWidget> {
               ),
               Padding(
                 padding: const EdgeInsets.all(10.0),
-                child: TextFormField(
-                  controller: schoolEditingController,
-                  onChanged: (text){
-                    setState(() {
-                      school=text;
-                    });
-                    },
-                  decoration: const InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(
-                              Radius.circular(20)),
-                          borderSide: BorderSide(color: Colors.deepPurple,width:2.0)
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(
-                              Radius.circular(20)),
-                          borderSide: BorderSide(color: Colors.deepPurple,width:2.0)
-                      ),
-                      fillColor: Colors.white,
-                      filled: true,
-                      labelText:'School'
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextFormField(
-                  controller: ageEditingController,
-                  onChanged: (text){
-                    setState(() {
-                      age=text;
-                    });
-                    },
-                  decoration: const InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(
-                              Radius.circular(20)),
-                          borderSide: BorderSide(color: Colors.deepPurple,width:2.0)
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(
-                              Radius.circular(20)),
-                          borderSide: BorderSide(color: Colors.deepPurple,width:2.0)
-                      ),
-                      fillColor: Colors.white,
-                      filled: true,
-                      labelText:'Age'
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextFormField(
-                  controller: cpaEditingController,
-                  onChanged: (text){
-                    setState(() {
-                                    cpa=text;
-                                  });
-                                },
-                  decoration: const InputDecoration(
-                                    enabledBorder: OutlineInputBorder(
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(20)),
-                                        borderSide: BorderSide(color: Colors.deepPurple,width:2.0)
-                                    ),
-                                    focusedBorder: OutlineInputBorder(
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(20)),
-                                        borderSide: BorderSide(color: Colors.deepPurple,width:2.0)
-                                    ),
-                                    fillColor: Colors.white,
-                                    filled: true,
-                                    labelText:'CPA'
-                                ),
-                              ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
                 child: Container(
                   height: heightR/14,
                   decoration:BoxDecoration(
                       color: Colors.white,
-                      border: Border.all(color: Colors.deepPurple,width: 2),
+                      border: Border.all(color: Colors.black,width: 2),
                       borderRadius: BorderRadius.circular(20)
                   ),
                   child: Row(
@@ -194,7 +102,7 @@ class _MyProfileWidgetState extends State<MyProfileWidget> {
                             setState(() {
                               gender_choose = newGender;
                             });
-                            } ,
+                          } ,
                           items: GenderList.map((gender){
                             return DropdownMenuItem(
                               value: gender,
@@ -208,22 +116,143 @@ class _MyProfileWidgetState extends State<MyProfileWidget> {
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.symmetric(vertical:10.0,horizontal:130),
-                child: Container(
-                  color: Colors.deepPurple[100],
-                  width: widthR/13,
-                  child: TextButton(
-                    child: const Text(
-                      'Update',
-                      style: TextStyle(
-                          color: Colors.black
+                padding: const EdgeInsets.all(10.0),
+                child: TextFormField(
+                  controller: ageEditingController,
+                  onChanged: (text){
+                    setState(() {
+                      age=text;
+                    });
+                  },
+                  decoration: const InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                              Radius.circular(20)),
+                          borderSide: BorderSide(color: Colors.black,width:2.0)
                       ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                              Radius.circular(20)),
+                          borderSide: BorderSide(color: Colors.deepPurple,width:2.0)
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                      labelText:'Age'
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextFormField(
+                  controller: schoolEditingController,
+                  onChanged: (text){
+                    setState(() {
+                      school=text;
+                    });
+                  },
+                  decoration: const InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                              Radius.circular(20)),
+                          borderSide: BorderSide(color: Colors.black,width:2.0)
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                              Radius.circular(20)),
+                          borderSide: BorderSide(color: Colors.deepPurple,width:2.0)
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                      labelText:'School'
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextFormField(
+                  controller: cpaEditingController,
+                  onChanged: (text){
+                    setState(() {
+                      cpa=text;
+                    });
+                  },
+                  decoration: const InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                              Radius.circular(20)),
+                          borderSide: BorderSide(color: Colors.black,width:2.0)
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                              Radius.circular(20)),
+                          borderSide: BorderSide(color: Colors.deepPurple,width:2.0)
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                      labelText:'CPA'
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextFormField(
+                  controller: emailEditingController,
+                  onChanged: (text){
+                    setState(() {
+                      _email=text;
+                    });
+                    },
+                  decoration: const InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                              Radius.circular(20)),
+                          borderSide: BorderSide(color: Colors.black,width:2.0)
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                              Radius.circular(20)),
+                          borderSide: BorderSide(color: Colors.deepPurple,width:2.0)
+                                    ),
+                                    fillColor: Colors.white,
+                                    filled: true,
+                                    labelText:'Email Address'
+                                ),
+                              ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical:10.0,horizontal:60),
+                child: Container(
+                  width: widthR/13,
+                  decoration: const BoxDecoration(
+                    color: Colors.deepPurple,
+                    borderRadius: BorderRadius.all(Radius.circular(20))
+                  ),
+                  child: TextButton(
+                    child:Text(
+                      'SAVE',
+                      style: GoogleFonts.roboto(fontSize: 20,color:Colors.white)
                     ),
                     onPressed: (){
-                      setState(() {
-                        _password='';
-                        _confirmpassword='';
-                      });
+                      User(_email,address,school, age, cpa,gender_choose);
+                      final snackBar = SnackBar(
+                        backgroundColor:Colors.purple[100],
+                        content: const Text('Wait a minutes...'),
+                        action: SnackBarAction(
+                          label: 'Undo',
+                          textColor: Colors.black,
+                          onPressed: () {
+                            setState(() {
+                              load=true;
+                            });
+                          },
+                        ),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                      if(load==false){
+                        Future.delayed(const Duration(milliseconds: 4650)).then((value){
+                          Navigator.pop(context);
+                        });
+                      }
                       },
                   ),
                 ),
@@ -238,9 +267,9 @@ class _MyProfileWidgetState extends State<MyProfileWidget> {
       child: Stack(
         children: [
           CircleAvatar(
-            radius:80.0,
-            backgroundImage:_selectedImage==null?const AssetImage('assets/images/avatarimage.png'):FileImage(File(_selectedImage!.path))as ImageProvider ,
-          ),
+                radius:80.0,
+                backgroundImage:_selectedImage==null?const AssetImage('assets/images/avatarimage.png'):FileImage(File(_selectedImage!.path))as ImageProvider ,
+              ),
           Positioned(
             top:120,
             left:113,
