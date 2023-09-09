e21bc1997bd0051778cfc9db19645a210f2d68cb
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2023-09-09 15:09:40 +0700

    add status bar color and initalizeApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,34 @@
-import 'package:app/Widgets/splash.dart';
+import 'package:app/pages/splash.dart';
+import 'package:app/models/user.dart';
+import 'package:app/services/auth.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:flutter/services.dart';
-void main(){
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+import 'package:app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'package:get/get.dart';
+void main()async{
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.white, // status bar color
   ));
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Myscreen(),
+    return MultiProvider(
+      providers: [
+        StreamProvider<MyUser?>.value(value: AuthService().user,initialData: null),
+      ],
+      child: const GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: Myscreen(),
+        ),
     );
   }
 }
