71984ead35133efaee89f8527ab0e958ff5ab144
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2023-09-13 15:09:22 +0700

    update model

--- lib/models/time_study.dart
+++ lib/models/time_study.dart
@@ -1,4 +1,4 @@
 class TimeStudy{
-  String? hour,minutes,name,subjects,owner;
-  TimeStudy({this.hour,this.minutes,this.name,this.subjects,this.owner});
+  String? hour,minutes,name,subjects,owner,loading,description,add;
+  TimeStudy({this.hour,this.minutes,this.name,this.subjects,this.owner,this.loading,this.description,this.add});
 }
