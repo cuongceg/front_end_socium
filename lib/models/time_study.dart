3f7e0da168ba10235a96a6007a6bc3fdcb05a2c3
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2023-09-14 15:03:06 +0700

    add comment

--- lib/models/time_study.dart
+++ lib/models/time_study.dart
@@ -2,3 +2,5 @@ class TimeStudy{
   String? hour,minutes,name,subjects,owner,loading,description,add;
   TimeStudy({this.hour,this.minutes,this.name,this.subjects,this.owner,this.loading,this.description,this.add});
 }
+//add is state symbolizes you swipe left or right.Swipe right is yes.Swipe left is no
+//loading is state symbolizes you watched or not.True is watched.False is not watched
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 class Auth{
-  final String? name,username,adress,school,age,cpa,gender;
-  Auth({this.adress, this.school, this.age, this.cpa, this.gender,this.name,this.username});
-}
+  final String? name,username,adress,school,age,cpa,gender,uid;
+  Auth({this.adress, this.school, this.age, this.cpa, this.gender,this.name,this.username,this.uid});
+}// models for profile
+//uid to access authList
+
 class MyUser{
   String? uid;
   MyUser({this.uid});
-}
+}//unique uid when user sign up
